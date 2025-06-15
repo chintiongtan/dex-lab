15a8a94404d0a3c61e931361a0942bccf3b5c4f6
Chin Tiong <alvin.tanct@gmail.com>
2025-06-15 20:57:52 +0800

    feat: added multi zone implementation

--- mfe-nextjs/packages/app-host/next.config.ts
+++ mfe-nextjs/packages/app-host/next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
+const { APP_SHOP_URL } = process.env;
+
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      /**
+       * @NOTE dlb_2-step_2: rewrites for [multi-zones](https://nextjs.org/docs/app/guides/multi-zones)
+       */
+      {
+        source: '/shop/:path*',
+        destination: `${APP_SHOP_URL}/:path*`,
+      },
+      {
+        source: '/shop-static/:path*',
+        destination: `${APP_SHOP_URL}/shop-static/:path*`,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
--- mfe-nextjs/packages/app-shop/src/app/page.tsx
+++ mfe-nextjs/packages/app-shop/src/app/page.tsx
@@ -49,6 +49,13 @@ export default function Home() {
           >
             Read our docs
           </a>
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            href="/"
+            rel="noopener noreferrer"
+          >
+            Go to host
+          </a>
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
