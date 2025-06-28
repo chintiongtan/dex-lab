5da47e7a25e098d764c5b21cb176d226ce2a8ef8
Chin Tiong <7303575+chintiongtan@users.noreply.github.com>
2025-06-28 15:32:27 +0800

    [DLB-2] Explore Next.js microfrontends (#2)
    * chore: setup monorepo
    
    * chore: created 2 nextjs app
    
    * feat: added multi zone implementation

---
+++ mfe-nextjs/packages/app-shop/next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  /**
+   * @NOTE dlb_2-step_1: setup asset prefix for remote app
+   */
+  assetPrefix: '/shop-static'
+};
+
+export default nextConfig;
